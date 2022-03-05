8b15e7e35726c82be51a61f347c9aa5cc33a7193
Bas Pieren <bas.pieren@quicknet.nl>
2022-03-05 16:21:49 +0100

    set eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,18 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended',
-  ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
+	root: true,
+	env: {
+		browser: true,
+		node: true
+	},
+	extends: [
+		'@nuxtjs/eslint-config-typescript',
+		'plugin:nuxt/recommended'
+	],
+	plugins: [
+	],
+	// add your custom rules here
+	rules: {
+		'no-tabs': 0,
+		indent: [1, 'tab']
+	}
 }
